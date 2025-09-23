86ef8721a6e80cabc6fb4375cfb091f00889b1fe
Caden Ream <cadenream@cadencoaster.com>
2025-09-23 07:56:48 -0700

    branding changes

--- src/main.rs
+++ src/main.rs
@@ -401,7 +401,7 @@ background-size: 400% 400%;
                             p { "Welcome to my blog! I am primarily doing this for school.. but will likely continue posting outside of school. I enjoy talking about tech stuff mainly software. But I also enjoy talking about robotics, hardware and science. I hope you find this useful or entertaining somewhat. :)" }
                             hr;
                             ul {
-                                li { a class = "btn-tag" hx-vals=r#"{"tag": "hardware"}"# hx-get="/posts" hx-target=".content" hx-swap="innerHTML" { "Hardware" } }
+                                li { a class = "btn-tag" hx-vals=r#"{"tag": "robotics"}"# hx-get="/posts" hx-target=".content" hx-swap="innerHTML" { "Robotics" } }
                                 li { a class = "btn-tag" hx-vals=r#"{"tag": "software"}"# hx-get="/posts" hx-target=".content" hx-swap="innerHTML" { "Software" } }
                                 li { a class = "btn-tag" hx-vals=r#"{"tag": "gaming"}"# hx-get="/posts" hx-target=".content" hx-swap="innerHTML" { "Gaming" } }
                                 li { a class = "btn-tag" hx-vals=r#"{"tag": "science"}"# hx-get="/posts" hx-target=".content" hx-swap="innerHTML" { "Science" } }
